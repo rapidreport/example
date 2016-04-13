7c168d22b3aaf6d1c0547f497a63f579062714bd
ykoji <ykoji@systembase.co.jp>
2016-04-13 09:36:20 +0900

    cs_pdf2追加

---
+++ cs_pdf2/example/FmMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace example
+{
+    public partial class FmMenu : Form
+    {
+        public FmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExample1_Click(object sender, EventArgs e)
+        {
+            Example1.Run();
+        }
+
+        private void BtnOpenOutput_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("output\\");
+        }
+
+    }
+}
