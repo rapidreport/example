1d2ae64d114fe8f26645a0e7d04f718ef280c8be
shuetsu <shuetsu@systembase.co.jp>
2021-07-06 13:50:46 +0900

    v5.10

--- java/src/example/ReadUtil.java
+++ java/src/example/ReadUtil.java
@@ -1,5 +1,33 @@
 package example;
 
+/*
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ReadUtil {
+
+	private ReadUtil(){};
+
+	public static Map<?, ?> readJson(String path) throws IOException{
+		InputStream is = new FileInputStream(path);
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+			return mapper.readValue(is, new TypeReference<LinkedHashMap<String,Object>>(){});
+		}finally {
+			is.close();
+		}
+	}
+}
+*/
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,14 +40,16 @@ import net.arnx.jsonic.JSONException;
 
 public class ReadUtil {
 
-	private ReadUtil(){};
+	private ReadUtil() {};
 
-	public static Map<?, ?> readJson(String path) throws JSONException, IOException{
+	public static Map<?, ?> readJson(String path) throws JSONException, IOException {
 		InputStream is = new FileInputStream(path);
-		try{
+		try {
 			Reader r = new InputStreamReader(is, "UTF-8");
 			try {
-				return (Map<?, ?>)JSON.decode(r);
+				JSON json = new JSON();
+				json.setMaxDepth(256);
+				return (Map<?, ?>) JSON.decode(r);
 			} finally {
 				r.close();
 			}
